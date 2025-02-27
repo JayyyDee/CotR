6fd5663c867f540bb5721430bc4e9ca42621a328
Redxilef <felixjacob1@hotmail.com>
2025-02-26 19:46:53 -0500

    Wwise Jadeite et Walk Rock
    L'objet Player contient les bruits de marche sur roche
    
    L'objet Gemme Jadéite contient les sons lorsqu'on récupère la Jadéite.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,33 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID EVENT_FOOTSTEP = 3618206844U;
+        static const AkUniqueID EVENT_JADEIDE_SLOW__PICKUP = 35105690U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID STONE = 1216965916U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK_GEMME_JADEITE_PICKUP = 2811913676U;
+        static const AkUniqueID SOUNDBANK_MOVEMENT = 486013808U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
