a89b19d43a263ba11d73dc0493455108a844ba6b
JayyyDee <hamelinogg@gmail.com>
2025-03-21 21:01:47 -0400

    Première implémentation du relay system

--- Assets/Scripts/MultiplayerScripts/GameMultiplayer.cs
+++ Assets/Scripts/MultiplayerScripts/GameMultiplayer.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -19,9 +24,60 @@ public class GameMultiplayer : NetworkBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    public void StartHost() {
+    //This fonction access the unity relay system to create a server that anyone on the planet can join.
+    private async Task<Allocation> AllocateRelay() {
+        try {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(MAX_PLAYER_AMOUNT - 1);
+
+            return allocation;
+        } catch (RelayServiceException e) {
+            Debug.Log(e);
+            return default;
+        }
+    }
+
+    private async Task<string> GetRelayJoinCode(Allocation allocation) {
+        try {
+            string relayJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            return relayJoinCode;
+        } catch (RelayServiceException e) {
+            Debug.Log(e);
+            return default;
+        }
+    }
+
+    private async Task<JoinAllocation> JoinRelay(string joinCode) {
+        try {
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            return joinAllocation;
+        } catch (RelayServiceException e) {
+            Debug.Log(e);
+            return default;
+        }
+
+    }
+
+    //Allocate the relay, create the join code then creates a connection by going through the Unity Relay system.
+    public async void StartHost() {
+        
+        Allocation allocation = await AllocateRelay();
+
+        string relayJoinCode = await GetRelayJoinCode(allocation);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+
         NetworkManager.Singleton.ConnectionApprovalCallback += NetworkManager_ConnectionApprovalCallback;
         NetworkManager.Singleton.StartHost();
+    }
+
+    //Get the join code from the allocation then connects to the created game.
+    public async void StartClient(string joinCode) {
+        OnTryingToJoinGame?.Invoke(this, EventArgs.Empty);
+
+        JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+
+        NetworkManager.Singleton.OnClientDisconnectCallback += NetworkManager_OnClientDisconnectCallback;
+        NetworkManager.Singleton.StartClient();
     }
 
     //This fonction makes it that the game doesnt allow late join and creates the player object.
@@ -37,13 +93,6 @@ public class GameMultiplayer : NetworkBehaviour
             return;
         }
         connectionApprovalResponse.Approved = true;
-    }
-
-    public void StartClient() {
-        OnTryingToJoinGame?.Invoke(this, EventArgs.Empty);
-
-        NetworkManager.Singleton.OnClientDisconnectCallback += NetworkManager_OnClientDisconnectCallback;
-        NetworkManager.Singleton.StartClient();
     }
 
     private void NetworkManager_OnClientDisconnectCallback(ulong clientID) {
--- Assets/Scripts/UIScripts/TestingLobbyUI.cs
+++ Assets/Scripts/UIScripts/TestingLobbyUI.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TestingLobbyUI : MonoBehaviour
-{
-    [SerializeField] private Button createGameButton;
-    [SerializeField] private Button joinGameButton;
+public class TestingLobbyUI : MonoBehaviour {
+    //    [SerializeField] private Button createGameButton;
+    //    [SerializeField] private Button joinGameButton;
 
-    private void Awake() {
-        //When clicking on the HOST button, start hosting a game.
-        createGameButton.onClick.AddListener(() => {
-            GameMultiplayer.Instance.StartHost();
-            Loader.LoadNetwork(Loader.Scene.CharacterLobbyScene);
-        });
+    //    private void Awake() {
+    //        //When clicking on the HOST button, start hosting a game.
+    //        createGameButton.onClick.AddListener(() => {
+    //            GameMultiplayer.Instance.StartHost();
+    //            Loader.LoadNetwork(Loader.Scene.CharacterLobbyScene);
+    //        });
 
-        //When clicking on the CLIENT button, join a game.
-        joinGameButton.onClick.AddListener(() => {
-            GameMultiplayer.Instance.StartClient();
-        });
-    }
+    //        //When clicking on the CLIENT button, join a game.
+    //        joinGameButton.onClick.AddListener(() => {
+    //            GameMultiplayer.Instance.StartClient();
+    //        });
+    //    }
 }
