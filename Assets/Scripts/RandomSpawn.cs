e219e21d2378a89c76a30142fc7310b372594a44
Thomas Hayaud <thomas.hayaud@gmail.com>
2025-03-24 12:46:56 -0400

    Debut du systeme de spawn

--- Assets/Scripts/RandomSpawn.cs
+++ Assets/Scripts/RandomSpawn.cs
@@ -2,34 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Unity.Netcode.Samples.APIDiorama.RPC
+void Start()
 {
-    /// <summary>
-    /// Manages how a player will be spawned
-    /// </summary>
-    class PlayerSpawnManager : NetworkBehaviour
-    {
-        void Start()
-        {
-            NetworkManager.ConnectionApprovalCallback = ConnectionApprovalCallback;
-        }
+    Vector3[] positionArray = new Vector3[6];
 
-        void ConnectionApprovalCallback(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
-        {
-            /* you can use this method in your project to customize one of more aspects of the player
-             * (I.E: its start position, its character) and to perform additional validation checks. */
-            response.Approved = true;
-            response.CreatePlayerObject = true;
-            response.Position = GetPlayerSpawnPosition();
-        }
+    positionArray[0] = new Vector3(-19f, -1f, 0f);
+    positionArray[1] = new Vector3(18f, 21f, 0f);
+    positionArray[2] = new Vector3(18f, -21f, 0f);
+    positionArray[3] = new Vector3(-10f, 21f, 0f);
+    positionArray[4] = new Vector3(26f, -1f, 0f);
+    positionArray[5] = new Vector3(-10f, -21f, 0f);
 
-        Vector3 GetPlayerSpawnPosition()
-        {
-            /*
-             * this is just an example, and you change this implementation to make players spawn on specific spawn points
-             * depending on other factors (I.E: player's team)
-             */
-            return new Vector3(Random.Range(-3, 3), 0, Random.Range(-3, 3));
-        }
-    }
+    //Player.transform.position = positionArray[Random.Range(0, positionArray.Length)];
 }
+
+//public static void Shuffle<T>
