b341a5d4840186c0f04b10581a6e1472642c7f81
JayyyDee <hamelinogg@gmail.com>
2025-03-26 11:41:03 -0400

    ]CA MARCHE UN PEU^^

--- Assets/Scripts/Aiming.cs
+++ Assets/Scripts/Aiming.cs
@@ -3,22 +3,17 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class Aiming : NetworkBehaviour
-{
+public class Aiming : NetworkBehaviour {
     private Vector3 mousePos;
     public Camera cam;
-    
-    
 
-    void FixedUpdate()
-    {
-        
+    void FixedUpdate() {
+
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-         Vector3 aimDirection = mousePos - transform.position;
-         float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-         transform.rotation = Quaternion.Euler(0, 0, aimAngle); 
-        
-        
+        Vector3 aimDirection = mousePos - transform.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, aimAngle);
+
         SetPlayerAimServerRpc(aimAngle);
     }
 
@@ -29,15 +24,11 @@ public class Aiming : NetworkBehaviour
 
     [ClientRpc]
     private void SetPlayerAimClientRpc(float aim) {
-
-        if (IsOwner)
-        {
+        if (!IsOwner) {
 
             transform.rotation = Quaternion.Euler(0, 0, aim);
         }
-        
-    
-        
-
     }
 }
+
+
