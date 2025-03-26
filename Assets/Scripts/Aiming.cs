a22412a2ddf03146ea8d3cb79a877349ccf95b1b
JayyyDee <hamelinogg@gmail.com>
2025-03-25 23:58:12 -0400

    Aim

--- Assets/Scripts/Aiming.cs
+++ Assets/Scripts/Aiming.cs
@@ -8,14 +8,23 @@ public class Aiming : NetworkBehaviour
     private Vector3 mousePos;
     public Camera cam;
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        SetPlayerAimServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SetPlayerAimServerRpc() {
+        SetPlayerAimClientRpc();
+    }
+
+    [ClientRpc]
+    private void SetPlayerAimClientRpc() {
         //if (IsOwner) {
-            mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 aimDirection = mousePos - transform.position;
-            float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, aimAngle);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 aimDirection = mousePos - transform.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, aimAngle);
         //}
     }
 }
