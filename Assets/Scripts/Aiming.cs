d856913f95e419111c5ac75273c57526756979cb
nyneo <slauze17@hotmail.com>
2025-03-28 11:37:33 -0400

    testststt

--- Assets/Scripts/Aiming.cs
+++ Assets/Scripts/Aiming.cs
@@ -15,10 +15,9 @@ public class Aiming : NetworkBehaviour {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 aimDirection = mousePos - transform.position;
         float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-        if (gameObject.transform.parent.GetComponent<NetworkObject>().IsOwner)
-        {
+        
             transform.rotation = Quaternion.Euler(0, 0, aimAngle);
-        }
+        
         //SetPlayerAimServerRpc(aimAngle);
     }
 
