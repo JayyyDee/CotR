27ecc7e582a7684111aaac0c3b0a690ca2ae7b8e
Redxilef <felixjacob1@hotmail.com>
2025-04-04 20:42:21 -0400

    ¡Viva la Música!
    Ajout de la musique Menu, Lobby, Game, selon leurs scènes.
    
    Pour ce faire j'ai créer un nouvelle objet qui ne ce destroy pas entre les scènes avec un script qui active et désactive les musiques selon la scène qui est active

---
+++ Assets/Scripts/Musics/MusicManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+
+    private uint musicPlayingID;
+    private string currentMusicEvent = "";
+
+    void Awake()
+    {
+        // Singleton pour ne pas dupliquer
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void Start()
+    {
+        HandleScene(SceneManager.GetActiveScene().name);
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        HandleScene(scene.name);
+    }
+
+    private void HandleScene(string sceneName)
+    {
+        switch (sceneName)
+        {
+            case "MainMenuScene": //Scene1
+                SwitchMusic("Play_Musique_Menu_Loop__itemnumber"); //Musique1
+                break;
+
+            case "LobbyScene": //Scene2
+            case "CharacterLobbyScene": //Scene3
+                SwitchMusic("Play_Musique_Lobby_Full_Onetime__itemnumber"); //Musique2
+                break;
+
+            case "GameScene": //Scene4
+                SwitchMusic("Play_Musique_Combat_Full_Onetime__itemnumber"); //Musique3
+                break;
+
+            default:
+                // Tu peux ajouter un event de musique par défaut si nécessaire
+                break;
+        }
+    }
+
+    private void SwitchMusic(string newMusicEvent)
+    {
+        if (currentMusicEvent == newMusicEvent)
+            return;
+
+        // Stop la musique actuelle si elle existe
+        if (!string.IsNullOrEmpty(currentMusicEvent))
+        {
+            AkUnitySoundEngine.StopPlayingID(musicPlayingID);
+        }
+
+        // Joue la nouvelle
+        musicPlayingID = AkUnitySoundEngine.PostEvent(newMusicEvent, gameObject);
+        currentMusicEvent = newMusicEvent;
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
