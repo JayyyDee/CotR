86547f94cc96982228b750dcd105736ee2dfb4a9
JayyyDee <hamelinogg@gmail.com>
2025-04-07 21:44:38 -0400

    Son ring percévérence

--- Assets/Scripts/Rings/RingPerseverance.cs
+++ Assets/Scripts/Rings/RingPerseverance.cs
@@ -75,7 +75,7 @@ public class RingPerseverance : Ring
         canFire = false;
         Vector3 pos = firePoint.transform.position;
         Quaternion rot = bulletPrefab.transform.rotation* firePoint.transform.rotation  ;
-
+        AkUnitySoundEngine.PostEvent("Play_FULL_Anneaux_Perseverence_Attack_TIR_FULL__itemnumber", this.gameObject);
         bulletPrefab.GetComponent<SpriteRenderer>().color = bulletColor;
         GameObject bullet = Instantiate(bulletPrefab, pos, rot);
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.transform.right * fireForce, ForceMode2D.Impulse);
@@ -85,6 +85,8 @@ public class RingPerseverance : Ring
     {
         Vector3 pos = playerCharacter.transform.position;
         Quaternion rot = playerCharacter.transform.rotation;
+        AkUnitySoundEngine.PostEvent("Play_FULL_Anneaux_Perseverence_Actif_Push_FULL__itemnumber", this.gameObject);
+        AkUnitySoundEngine.PostEvent("Play_FULL_Anneaux_Perseverence_Actif_Start_FULL__itemnumber", this.gameObject);
         GameObject activeHitbox = Instantiate(activePrefab, pos, rot);
         activeHitbox.GetComponent<NetworkObject>().Spawn();
         activeHitbox.GetComponent<ActivePerseverance>().player = playerCharacter;
