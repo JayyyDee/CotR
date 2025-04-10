124010f67ff235c730d27ab4aa71efaf59bd63d0
nyneo <slauze17@hotmail.com>
2025-04-10 13:56:09 -0400

    Test Network

--- Assets/Scripts/Projectiles/HitboxBravoure.cs
+++ Assets/Scripts/Projectiles/HitboxBravoure.cs
@@ -5,19 +5,11 @@ using UnityEngine;
 public class HitboxBravoure : MonoBehaviour
 {
     public int damage;
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        //if (collision.collider.CompareTag("Enemy"))
-        //{
-
-            
-            Debug.Log("HIT");
-            collision.gameObject.GetComponent<HealthManager>().TakeDamageServerRpc(damage);
-
-            //Damage
-        //}
-
-
+        if (collision.GetComponent<Collider>().CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<HealthManager>().TakeDamageServerRpc(damage);    
+        }
     }
 }
